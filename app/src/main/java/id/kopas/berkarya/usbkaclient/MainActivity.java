8e21570c91bef3d097fdbb29002faf58d1ac86a2
EKO HENDRATNO <eko.hendratno@gmail.com>
2020-02-29 23:43:38 +0700

    1.1.1

--- app/src/main/java/id/kopas/berkarya/usbkaclient/MainActivity.java
+++ app/src/main/java/id/kopas/berkarya/usbkaclient/MainActivity.java
@@ -9,8 +9,11 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -35,6 +38,7 @@ import static id.kopas.berkarya.usbkaclient.DetectConnection.isNetworkStatusAvia
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "MainActivity";
     private static int PAGE_LOAD_PROGRESS = 0;
     private ConnectionTimeoutHandler timeoutHandler = null;
     private ProgressBar progressBar;
@@ -48,16 +52,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
-        String s = getResources().getString(R.string.url_server);
+        String alamat_server = getResources().getString(R.string.url_server);
 
         final WebView view = findViewById(R.id.view);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+        cekKebutuhanApp();
 
-        /**Intent i = getIntent();
+        /**
+        Intent i = getIntent();
         String alamat = i.getStringExtra("alamat");
         if(!alamat.equals("")){
-            s = alamat;
+            alamat_server = alamat;
         }*/
 
         view.getSettings().setJavaScriptEnabled(true);
@@ -73,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         view.setWebChromeClient(new MyWebChromeClient(this));
         //view.setWebChromeClient(new WebChromeClient());
 
-        view.loadUrl(s);
+        view.loadUrl(alamat_server);
         swipeRefreshLayout = findViewById(R.id.swipe);
         swipeRefreshLayout.setColorSchemeColors(Color.RED, Color.GREEN, Color.BLUE, Color.CYAN);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -87,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                     view.loadDataWithBaseURL(null, "<html><body><img width=\"100%\" height=\"100%\" src=\"file:///android_res/drawable/offline.png\"></body></html>", "text/html", "UTF-8", null);
                     //progressDialogModel.hideProgressDialog();
                     swipeRefreshLayout.setRefreshing(false);
-                    Intent i = new Intent(getBaseContext(), MasukActivity.class);
+                    Intent i = new Intent(getBaseContext(), OfflineActivity.class);
                     startActivity(i);
                     finish();
                 }
@@ -125,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 view.loadDataWithBaseURL(null, "<html><body><img width=\"100%\" height=\"100%\" src=\"file:///android_res/drawable/offline.png\"></body></html>", "text/html", "UTF-8", null);
                 //progressDialogModel.hideProgressDialog();
                 swipeRefreshLayout.setRefreshing(false);
-                Intent i = new Intent(getBaseContext(), MasukActivity.class);
+                Intent i = new Intent(getBaseContext(), OfflineActivity.class);
                 startActivity(i);
                 finish();
             }
@@ -147,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
             swipeRefreshLayout.setRefreshing(false);
             progressBar.setVisibility(View.GONE);
 
-            Intent i = new Intent(getBaseContext(), MasukActivity.class);
+            Intent i = new Intent(getBaseContext(), OfflineActivity.class);
             i.putExtra("valid", "offline");
             startActivity(i);
             System.exit(0);
@@ -372,5 +378,58 @@ public class MainActivity extends AppCompatActivity {
         } catch (PendingIntent.CanceledException e) {
             e.printStackTrace();
         }
+    }
+
+    private void cekKebutuhanApp() {
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo("com.google.android.webview", 0);
+            Log.d(TAG, "version name: " + pi.versionName);
+            Log.d(TAG, "version code: " + pi.versionCode);
+            try {
+                PackageInfo pi2 = pm.getPackageInfo("com.android.chrome", 0);
+                Log.d(TAG, "version name: " + pi2.versionName);
+                Log.d(TAG, "version code: " + pi2.versionCode);
+
+                int firstDotIndex = pi2.versionName.indexOf(".");
+                String majorVersion = pi2.versionName.substring(0, firstDotIndex);
+
+                if( Integer.parseInt(majorVersion) < 50 ){
+                    pesanDialog("Google Chrome versi lama, harap update dahulu?",getString(R.string.chrome) );
+                }
+
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e(TAG, "Google Chrome is not found");
+
+                pesanDialog("Google Chrome tidak tersedia, harap download dahulu?",getString(R.string.chrome) );
+
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Android System WebView is not found");
+
+            pesanDialog("Android System WebView tidak tersedia, harap download dahulu?",getString(R.string.webview) );
+
+        }
+    }
+
+    private void pesanDialog(String pesan, final String link){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("App dibutuhkan");
+        alertDialogBuilder.setMessage(pesan).setCancelable(false).setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(link)));
+
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        }).setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
+        alertDialogBuilder.create().show();
     }
 }
